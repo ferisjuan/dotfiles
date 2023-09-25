07ce3b4e6203856ebf6ee1541c7a3da0a4ece878
juan <juan>
2023-09-24 20:59:46 -0500

    add nvchad config

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,37 @@
+local config = require "plugins.configs.lspconfig"
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "tsserver" }
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    commands = {
+      OrganizeImports = {
+        organize_imports,
+        description = "Organize Imports",
+      },
+    },
+  }
+end
+
+-- lspconfig.tsserver.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   init_options ={
+--     preferences ={
+--       disablSuggestions = true
+--     }
+--   }
+-- }
