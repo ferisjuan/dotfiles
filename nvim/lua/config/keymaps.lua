a018edd26510a43f95ed732458e4cde881e96848
Juan Feris <feris.juan@gmail.com>
2024-05-05 15:16:25 -0500

    new nvim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps hereby
-
-vim.keymap.set("i", "˚", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-vim.keymap.set("i", "∆", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-vim.keymap.set("i", "jk", "<esc>", { desc = "Escape" })
-vim.keymap.set("n", "˚", "<cmd>m .-2<cr>==", { desc = "Move up" })
-vim.keymap.set("n", "∆", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("v", "<leader>cs", ":sort<cr>", { desc = "Sort" })
-vim.keymap.set("v", "˚", ":m '<-2<cr>gv=gv", { desc = "Move up" })
-vim.keymap.set("v", "∆", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+-- Add any additional keymaps here
