f7e3a5d1662b91e2eabb88cdf123bd4712514cff
Juan Feris <feris.juan@gmail.com>
2025-09-22 11:23:08 -0500

    update nvim colorscheme transparency

--- nvim/.config/nvim/lua/plugins/color-scheme.lua
+++ nvim/.config/nvim/lua/plugins/color-scheme.lua
@@ -1,10 +1,20 @@
+local opts = {
+  transparent_background = true,
+  transparent = true,
+  styles = { sidebars = "transparent", floats = "transparent" },
+}
+
 return {
   { "NLKNguyen/papercolor-theme", name = "papercolor", lazy = true },
-  { "bluz71/vim-nightfly-guicolors", name = "nightfly", lazy = true },
-  { "catppuccin/nvim", name = "catppuccin", lazy = true },
+  {
+    "bluz71/vim-nightfly-guicolors",
+    name = "nightfly",
+    lazy = true,
+  },
+  { "catppuccin/nvim", name = "catppuccin", lazy = true, opts = opts },
   { "ellisonleao/gruvbox.nvim", name = "gruvbox", lazy = true },
-  { "folke/tokyonight.nvim", name = "tokyonight", lazy = true },
-  { "rebelot/kanagawa.nvim", name = "kanagawa", lazy = true },
+  { "folke/tokyonight.nvim", name = "tokyonight", lazy = true, opts = opts },
+  { "rebelot/kanagawa.nvim", name = "kanagawa", lazy = true, opts = opts },
   { "rose-pine/neovim", name = "rose-pine", lazy = true },
   { "sainnhe/everforest", name = "everforrest", lazy = true },
 
@@ -12,7 +22,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfly",
+      colorscheme = "tokyonight",
     },
   },
 }
